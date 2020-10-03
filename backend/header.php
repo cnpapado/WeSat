b70805fa530c85241422e43af5bbaee4e789f298
jimmy <dgeorgiou3@gmail.com>
2020-10-03 16:28:00 +0300

    backend initial commit

---
+++ backend/header.php
@@ -0,0 +1,31 @@
+<head>
+    <title>NASA APP</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<style type="text/css">
+.brand{
+    background: #9923a1 !important;
+}
+.brand-text{
+    color: #9923a1 !important;
+}
+
+form{
+    max-width: 460px;
+    margin: 20px auto;
+    padding: 20px;
+}
+</style>
+
+</head>
+
+<body class="grey lighten-4">
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href=index.php class="brand-logo brand-text"> NASA APP</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li><a href="searchByName.php" class="btn brand z-depth-0">Search By Name</a></li>
+                <li><a href="addSatelight.php" class="btn brand z-depth-0">Add Satelight</a></li>
+            </ul>
+        </div>
+    </nav>
